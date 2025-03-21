3346fbc04469519f8c1d33ce62e565b7d4ea48fd
minthihalwin <minthihalwin.dev@gmail.com>
2025-03-22 02:11:37 +1300

    Feat: implement authentication in Navbar component and remove unused SVG files

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import { auth, signIn, signOut } from "@/auth";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await auth();
   return (
-    <div>Navbar</div>
-  )
-}
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image src="/logo.png" alt="logo" width={144} height={30} />
+        </Link>
 
-export default Navbar
+        <div className="flex items-center gap-5 text-black">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+
+              <form
+                action={async () => {
+                  "use server";
+
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit">logout</button>
+              </form>
+              <Link href={`/user/${session?.user?.id}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+
+                await signIn("github");
+              }}
+            >
+              <button type="submit">Login</button>
+            </form>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
